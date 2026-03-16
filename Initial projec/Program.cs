002e8c829a6482115fc0e8dd2a69b9d95120b0fe
Konrad Karczewski <s34434@pjwstk.edu.pl>
2026-03-16 17:36:08 +0100

    Test metody CalculateAverage w pliku Program.cs

--- Initial projec/Program.cs
+++ Initial projec/Program.cs
@@ -9,8 +9,10 @@ double wynik = StatisticsHelper.kwadrat(liczba);
 Console.WriteLine($"Wynik kwadratu podanej liczby : {wynik}");
 
 
+int[] liczby = { 1,2,6,23,53,1232} ;
 
-
+double srednia = StatisticsHelper.CalculateAverage(liczby);
+Console.WriteLine($"Srednia: {srednia}");
 
 
 
