eca591f0125db001420ea6533ca28a1dff3ad99e
Konrad Karczewski <s34434@pjwstk.edu.pl>
2026-03-16 15:40:05 +0100

    Dodanie klasy StatisticHelper oraz dodanie metody w tej klasie o nazwie: kwadrat, podnoszącą podaną liczbę do kwadratu

--- Initial projec/Program.cs
+++ Initial projec/Program.cs
@@ -1,3 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Console.WriteLine("Hello, World!");
 
-Console.WriteLine("Hello, World!");
+public class StatisticsHelper
+{
+
+    public static double kwadrat(double liczba)
+    {
+        return liczba * liczba;
+    }
+
+
+
+}
+
+
