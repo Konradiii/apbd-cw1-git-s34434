87368f2edb8a0e2c916250b457915869358cbae7
Konrad Karczewski <s34434@pjwstk.edu.pl>
2026-03-16 17:00:13 +0100

    Dodanie metody obliczającej odległość euklidesową.

--- Initial projec/StatisticsHelper.cs
+++ Initial projec/StatisticsHelper.cs
@@ -6,4 +6,20 @@ public class StatisticsHelper
         return liczba * liczba;
     }
 
+
+    public static double OdlegloscEuklidesowa(double x1, double y1, double x2, double y2)
+    {
+        
+        double dx =  x2 - x1;
+        double dy = y2 - y1;
+        
+        return Math.Sqrt(dx*dx + dy*dy);
+    }
+
+
+
+
+
+
+
 }
