07bfda912eecd4393372df0757b939b515ddef88
Konrad Karczewski <s34434@pjwstk.edu.pl>
2026-03-16 17:32:22 +0100

    Dodanie metody CalculateAvarage

--- Initial projec/StatisticsHelper.cs
+++ Initial projec/StatisticsHelper.cs
@@ -17,6 +17,21 @@ public class StatisticsHelper
     }
 
 
+    public static double CalculateAverage(int[]  values)
+    {
+
+     if (values.Length == 0 ) return 0;
+
+     int sum = 0;
+     foreach (var v in values)
+         sum += v;
+
+     return (double)sum / values.Length;
+
+
+
+    }
+
 
 
 
